db3bd37a4b31322132594ab73d0811e15c19a514
Daniel-Andrade-dev <dani.andrade@gmail.com>
2026-09-19 20:22:44 -0300

    Adicionado novo blueprint relatorio produtos

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 from api.routes.produto_routes import produto_bp
 from api.routes.venda_routes import venda_bp
 from api.routes.categoria_routes import categoria_bp
-from api.routes.relatorio_routes import relatorio_bp
-
+from api.routes.relatorio_venda_routes import relatorio_bp
+from api.routes.relatorio_produto_routes import relatorio_produto_bp
 
 app = Flask(__name__)
 # CORS(app)
@@ -13,6 +13,7 @@ app.register_blueprint(produto_bp)
 app.register_blueprint(venda_bp)
 app.register_blueprint(categoria_bp)
 app.register_blueprint(relatorio_bp)
+app.register_blueprint(relatorio_produto_bp)
 
 if __name__ == "__main__":
     app.run(
